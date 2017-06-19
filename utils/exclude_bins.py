d7bfcd843763b1d9c18e6a3f61a064553d75ceb4
Fernando Meyer <fernando.meyer@gmail.com>
2017-06-19 17:20:06 +0200

    add tool for excluding bins

--- utils/exclude_bins.py
+++ utils/exclude_bins.py
@@ -12,14 +12,15 @@ def load_unique_common(unique_common_file_path):
     return genome_to_unique_common
 
 
-def load_data(stream, unique_common_file_path):
+def load_data(stream, unique_common_file_path, keyword):
     genome_to_unique_common = load_unique_common(unique_common_file_path)
     for line in stream:
         line = line.strip()
         if len(line) == 0 or line.startswith("@"):
+            print line
             continue
         bin = line.split('\t')[0]
-        if bin in genome_to_unique_common and genome_to_unique_common[bin] == "circular element":
+        if bin in genome_to_unique_common and (keyword is None or genome_to_unique_common[bin] == keyword):
             continue
         print line
 
@@ -28,10 +29,9 @@ def main():
     parser = argparse.ArgumentParser(description="Exclude bins from file or standard input")
     parser.add_argument('file', nargs='?', type=argparse.FileType('r'), help="File containing precision and recall for each bin")
     parser.add_argument('-b', '--bins', help="File with list of bins", required=True)
-    parser.add_argument('-k', '--keyword', help="Keyword in second column of input for bins to be removed (none=remove all in list)", required=False)
+    parser.add_argument('-k', '--keyword', help="Keyword in second column of input for bins to be removed (no keyword=remove all in list)", required=False)
     args = parser.parse_args()
-    print args.keyword
-    if not args.bins and sys.stdin.isatty():
+    if not args.file and sys.stdin.isatty():
         parser.print_help()
         parser.exit(1)
     load_data(sys.stdin if not sys.stdin.isatty() else args.file,
