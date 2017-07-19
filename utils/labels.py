e1d98c6763eacee5db46f24ca6ae83f5a3ea2ec8
Fernando Meyer <fernando.meyer@gmail.com>
2017-07-19 16:38:48 +0200

    add dictionary of abbreviations to labels.py

--- utils/labels.py
+++ utils/labels.py
@@ -14,3 +14,23 @@ RI_BY_SEQ = "rand_index_by_seq"
 ARI_BY_BP = "a_rand_index_by_bp"
 ARI_BY_SEQ = "a_rand_index_by_seq"
 PERCENTAGE_ASSIGNED_BPS = "percent_assigned_bps"
+
+abbreviations = {'avg_precision': 'precision averaged over genome bins',
+                 'std_dev_precision': 'standard deviation of precision averaged over genome bins',
+                 'sem_precision': 'standard error of the mean of precision averaged over genome bins',
+                 'avg_recall': 'recall averaged over genome bins',
+                 'std_dev_recall': 'standard deviation of recall averaged over genome bins',
+                 'sem_recall': 'standard error of the mean of recall averaged over genome bins',
+                 'precision': 'precision weighed by base pairs',
+                 'recall': 'recall weighed by base pairs',
+                 'rand_index_by_bp': 'Rand index weighed by base pairs',
+                 'rand_index_by_seq': 'Rand index weighed by sequence counts',
+                 'a_rand_index_by_bp': 'adjusted Rand index weighed by base pairs',
+                 'a_rand_index_by_seq': 'adjusted Rand index weighed by sequence counts',
+                 'percent_assigned_bps': 'percentage of base pairs that were assigned to bins',
+                 '>0.5compl<0.1cont': 'number of genomes with more than 50% completeness and less than 10% contamination',
+                 '>0.7compl<0.1cont': 'number of genomes with more than 70% completeness and less than 10% contamination',
+                 '>0.9compl<0.1cont': 'number of genomes with more than 90% completeness and less than 10% contamination',
+                 '>0.5compl<0.05cont': 'number of genomes with more than 50% completeness and less than 5% contamination',
+                 '>0.7compl<0.05cont': 'number of genomes with more than 70% completeness and less than 5% contamination',
+                 '>0.9compl<0.05cont': 'number of genomes with more than 90% completeness and less than 5% contamination'}
