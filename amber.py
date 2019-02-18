fix accuracy per frag computation

amber.py
@@ -80,7 +80,7 @@ def compute_metrics_over_bins(rank, gs_pd_bins_rank, pd_bins_rank, query):
             true_positives_recall_seq += max(bin_assigns_seqs)
 
     gs_length = gs_pd_bins_rank['true_size'].sum()
-    gs_num_seqs = gs_pd_bins_rank['true_size'].sum()
+    gs_num_seqs = gs_pd_bins_rank['true_num_seqs'].sum()
     recall_bp = true_positives_recall_bp / gs_length
     recall_seq = true_positives_recall_seq / gs_num_seqs
     accuracy_bp = true_positive_bps_all_bins / gs_length
@@ -123,9 +123,9 @@ def compute_percentage_of_assigned_seqs(gold_standard, query):
                 percentage_of_overbinned_seqs[rank] = len(query.rank_to_overbinned_seqs[rank]) / gs_num_seqs[rank]
         for rank in load_ncbi_taxinfo.RANKS:
             if rank not in percentage_of_assigned_seqs:
-                percentage_of_assigned_seqs[rank] = np.nan
+                percentage_of_assigned_seqs[rank] = .0
             if rank not in percentage_of_overbinned_seqs:
-                percentage_of_overbinned_seqs[rank] = np.nan
+                percentage_of_overbinned_seqs[rank] = .0
     return percentage_of_assigned_seqs, percentage_of_overbinned_seqs
 
 
@@ -170,7 +170,6 @@ def evaluate_all(gold_standard,
 
             avg_precision_bp = precision_bp_rows.mean()
             sem_precision_bp = precision_bp_rows.sem()
-
             avg_recall_bp = recall_bp_rows.mean()
             sem_recall_bp = recall_bp_rows.sem()
 

src/binning_classes.py
@@ -385,7 +385,7 @@ class TaxonomicBin(Bin):
         if len(self.mapping_id_to_length) == 0:
             self.compute_confusion_matrix(gold_standard)
         gold_standard_query = gold_standard.taxonomic_query
-        if not self.id in gold_standard_query.get_bin_ids():
+        if self.id not in gold_standard_query.get_bin_ids():
             return
         gs_bin = gold_standard_query.get_bin_by_id(self.id)
         commmon_seq_ids = self.sequence_ids & gs_bin.sequence_ids
@@ -402,7 +402,7 @@ class TaxonomicBin(Bin):
             true_size = true_num_seqs = np.nan
         return {'id': self.id,
                 'name': gold_standard_query.tax_id_to_name[self.id] if gold_standard_query.tax_id_to_name else np.nan,
-                'rank': gold_standard_query.tax_id_to_rank[self.id],
+                'rank': self.rank,
                 'mapping_id': self.id,
                 'purity_bp': self.precision_bp,
                 'purity_seq': self.precision_seq,